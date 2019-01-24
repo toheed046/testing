ef2540804c2cc0554605cff4ea368759b400ff61
SANDEEP RAWAT <SANDEEP RAWAT@SANDEEP>
2019-01-24 18:07:45 +0530

    added setters and getters of status field

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -3,5 +3,13 @@ package testing;
 public class Test {
 
 	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 	
 }
